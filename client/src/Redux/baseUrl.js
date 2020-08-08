e22cec8d762f5c0f37ce460fc2222b20b728a6cf
Ethical-Ralph <rakinola90@gmail.com>
2020-08-08 22:48:46 +0100

    fix: minor fix

--- client/src/Redux/baseUrl.js
+++ client/src/Redux/baseUrl.js
@@ -1,3 +1,3 @@
-// export default "http://localhost:4000/v1";  /
+// export default "http://localhost:4000/v1";
 // export default "http://172.20.10.3:4000/v1";
 export default "https://mytodomonitor.herokuapp.com/v1";
